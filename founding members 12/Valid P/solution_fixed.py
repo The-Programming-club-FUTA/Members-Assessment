aeeccbc89fdbf5d34e9afcad4d2ca354da245469
Abdulsalam Akinyoola <abdulsalamakinyoola@gmail.com>
2026-01-09 17:12:56 +0100

    fix: validate remaining open brackets in parentheses check

---
+++ founding members 12/Valid P/solution_fixed.py
@@ -0,0 +1,19 @@
+def isValid(self, s: str) -> bool:
+    open_brackets = {"(", "{", "["}
+    stack = []
+
+    for st in s:
+        if st in open_brackets:
+            stack.append(st)
+        else:
+            if not stack \
+                or (st == ")" and stack[-1] != "(") \
+                or (st == "}" and stack[-1] != "{") \
+                or (st == "]" and stack[-1] != "["):
+                return False
+            
+            stack.pop()
+
+    return len(stack) == 0
+
+
